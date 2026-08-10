e65ea344eab86c317961e9a5f784268684e6662f
Moctezuma557 <adrianpromaster75@gmail.com>
2026-08-09 22:29:04 -0600

    1- Se añadio la funcionalidad para el contructor y el método "cargar"

--- firmware/DinoStar REX v1 - Firmware/src/classes/ConfigManager.cpp
+++ firmware/DinoStar REX v1 - Firmware/src/classes/ConfigManager.cpp
@@ -1,4 +1,17 @@
-#include "ConfigManager.h"
+#include "ConfigManager.h" // <== Código de funcionalidad para la clase ConfigManager
 
 // Nombre del namespace en NVS — máximo 15 caracteres
 const char* ConfigManager::NAMESPACE = "rex_config";
+
+ConfigManager::ConfigManager(){
+    pacienteId = "Sin asignar";
+    VolTotal = 500.0f; // <== 500 ml como valor por defecto
+    modoGoteo = ModoGoteo::NORMAL_GOTEO;
+};
+
+ConfigManager::cargar(){
+    pref.begin(NAMESPACE, false);
+
+    pacienteId = pref.getString("pacienteId", "Sin asignar");
+    VolTotal = pref.getFloat("VolTotal", 500.0f);
+};
