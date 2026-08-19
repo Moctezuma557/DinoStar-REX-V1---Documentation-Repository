cc97051c99b3f26d972cba3739a2a4dd0ff04eb5
Moctezuma557 <adrianpromaster75@gmail.com>
2026-08-19 01:22:16 -0600

    Se crea archivo cpp, se añade funcionalidad de algunos metodos

---
+++ firmware/DinoStar REX v1 - Firmware/src/classes/VenoclisisSession.cpp
@@ -0,0 +1,90 @@
+#include "VenoclisisSession.h"
+
+
+// <========================  CONSTRUCTOR =========================>
+
+VenoclisisSession::VenoclisisSession(){
+    pacienteId = "Sin asignar";
+    inicioSesion = 0;
+    volTotal = 0.00f;
+    estado = EstadoSesion::FINALIZADA;
+};
+
+
+
+
+
+// <========================  MÉTODO Iniciar =========================>
+void VenoclisisSession::iniciar() {
+    if (estado == EstadoSesion::FINALIZADA) {
+        inicioSesion = millis();
+        estado = EstadoSesion::ACTIVA;
+    } else {
+        Serial.println("[VenoclisisSession] NO se puede iniciar, ya hay una sesión en curso");
+    }
+}
+
+
+// <========================  MÉTODO Pausar =========================>
+void VenoclisisSession::pausar() {
+
+    if(estado == EstadoSesion::ACTIVA){
+        estado = EstadoSesion::PAUSADA;
+    } 
+    else {
+        Serial.println("[VenoclisisSession] NO se puede pausar una sesión que no está activa");
+    }
+};
+
+
+// <========================  MÉTODO Terminar =========================>
+
+void VenoclisisSession::terminar() {
+    if (estado == EstadoSesion::ACTIVA || estado == EstadoSesion::PAUSADA) {
+        estado = EstadoSesion::FINALIZADA;
+    } else {
+        Serial.println("[VenoclisisSession] NO se puede terminar una sesión que no está activa");
+    }
+};
+
+// <========================  MÉTODO isActiva =========================>
+
+bool VenoclisisSession::isActiva() {
+    return (estado == EstadoSesion::ACTIVA || estado == EstadoSesion::PAUSADA);
+}
+
+
+
+// <========================  MÉTODOS get =========================>
+
+String VenoclisisSession::getPacienteId() const {
+    return pacienteId;
+};
+
+uint32_t VenoclisisSession::getInicioSesion() const {
+    return inicioSesion;
+};
+
+float VenoclisisSession::getVolTotal() const {
+    return volTotal;
+};
+
+EstadoSesion VenoclisisSession::getEstado() const {
+    return estado;
+};
+
+void VenoclisisSession::configurar(const String& id, float vol) {
+    pacienteId = id;
+    volTotal = vol;
+};
+
+void VenoclisisSession::debug() const {
+    Serial.println("---- VenoclisisSession ----");
+    Serial.println("  pacienteId : " + pacienteId);
+    Serial.println("  inicioSesion : " + String(inicioSesion));
+    Serial.println("  volTotal   : " + String(volTotal) + " mL");
+    Serial.println("  estado     : " + String(estado == EstadoSesion::ACTIVA
+                                               ? "ACTIVA"
+                                               : "PAUSADA"));
+    Serial.println("-----------------------");
+}
